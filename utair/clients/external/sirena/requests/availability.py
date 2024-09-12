1fdf642efa52e336b8c6d50f49096653a99b3b55
annaustiuzhanina <anna.ustyuzhanina@utair.ru>
2024-09-12 16:43:29 +0300

    availability request fix

--- utair/clients/external/sirena/requests/availability.py
+++ utair/clients/external/sirena/requests/availability.py
@@ -1,17 +1,17 @@
 from pydantic import Field
-from typing import Optional
+from typing import Optional, Union
 from datetime import date
 from utair.clients.external.sirena.base.models.base_client_request import RequestModelABC
 
 
-class GetAvailabilityRequest(RequestModelABC):
+class AvailabilityRequest(RequestModelABC):
     """
     Запрос используется для получения информации о наличии мест на направлении.
     """
 
     departure: str = Field(description="Код города или порта отправления")
     arrival: str = Field(description="Код города или порта прибытия")
-    date: Optional[date] = Field(default=None, description="Дата вылета")
+    departure_date: Optional[date] = Field(default=None, description="Дата вылета") # вот тут проблема
     company: Optional[str] = Field(default=None, description="Код авиакомпании")
     flight: Optional[str] = Field(default=None, description="Номер рейса")
     baseclass: Optional[str] = Field(default=None, description="Класс обслуживания")
@@ -22,10 +22,10 @@ class GetAvailabilityRequest(RequestModelABC):
     time_till: Optional[int] = Field(default=None, description="Максимальное время вылета")
 
     joint_type: str = Field(default='jtAll', description="Способ выбора стыковок")
-    check_tch_restrictions: Optional[bool] = Field(description="Признак обязательного учета ограничений на продажу в сеансе ТКП")
-    use_dag: Optional[bool] = Field(description="Признак обязательного учета ограничений на продажу по картотекам ДАР/ДАГ")
-    use_iak: Optional[bool] = Field(description="Признак обязательного учета ограничений на продажу по картотеке ИАК")
-    use_a02: Optional[bool] = Field(description="Признак необходимости использования таблицы А02 для определения класса обслуживания")
+    check_tch_restrictions: Optional[bool] = Field(default=False, description="Признак обязательного учета ограничений на продажу в сеансе ТКП")
+    use_dag: Optional[bool] = Field(default=False, description="Признак обязательного учета ограничений на продажу по картотекам ДАР/ДАГ")
+    use_iak: Optional[bool] = Field(default=False, description="Признак обязательного учета ограничений на продажу по картотеке ИАК")
+    use_a02: Optional[bool] = Field(default=False, description="Признак необходимости использования таблицы А02 для определения класса обслуживания")
 
     show_flighttime: Optional[bool] = Field(default=None, description="Получение информации о времени перелета")
     show_baseclass: Optional[bool] = Field(default=None, description="Выводить базовый класс для каждого подкласса в ответе")
@@ -35,6 +35,9 @@ class GetAvailabilityRequest(RequestModelABC):
 
     lang: str = 'en'
     _method_name: str = 'availability'
+
+    def format_date(self, dt: Union[date, None]) -> Union[str, None]:
+        return dt.strftime("%d.%m.%Y") if dt else None
 
     def build(self) -> dict:
         request_params = {
@@ -48,7 +51,7 @@ class GetAvailabilityRequest(RequestModelABC):
         answer_params = {
             "show_flighttime": self.show_flighttime,
             "show_baseclass": self.show_baseclass,
-            "return_date": self.return_date,
+            "return_date": self.format_date(self.return_date),
             "mark_cityport": self.mark_cityport,
             "show_et": self.show_et,
         }
@@ -56,7 +59,7 @@ class GetAvailabilityRequest(RequestModelABC):
         request = {
             "departure": self.departure,
             "arrival": self.arrival,
-            "date": self.date,
+            "date": self.format_date(self.departure_date),
             "company": self.company,
             "flight": self.flight,
             "baseclass": self.baseclass,
@@ -68,4 +71,8 @@ class GetAvailabilityRequest(RequestModelABC):
             "request_params": request_params,
             "answer_params": answer_params,
         }
+
+        if self.direct is not None:
+            request["direct"] = self.direct
+
         return request
